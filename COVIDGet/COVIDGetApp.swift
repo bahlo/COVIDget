54408e2796146ec4b3c9813c270f190117ab2db6
Arne Bahlo <hallo@arne.me>
2020-09-21 13:03:15 +0200

    Initial Commit

---
+++ COVIDGet/COVIDGetApp.swift
@@ -0,0 +1,17 @@
+//
+//  COVIDGetApp.swift
+//  COVIDGet
+//
+//  Created by Arne Bahlo on 21.09.20.
+//
+
+import SwiftUI
+
+@main
+struct COVIDGetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ COVIDGet/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  COVIDGet
+//
+//  Created by Arne Bahlo on 21.09.20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
