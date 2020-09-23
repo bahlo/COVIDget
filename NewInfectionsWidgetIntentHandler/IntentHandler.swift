f6035a4459580b572db244c433b63e14fcc62900
Arne Bahlo <hallo@arne.me>
2020-09-23 10:30:51 +0200

    Sort districts

--- NewInfectionsWidgetIntentHandler/IntentHandler.swift
+++ NewInfectionsWidgetIntentHandler/IntentHandler.swift
@@ -10,10 +10,13 @@ import Intents
 class IntentHandler: INExtension, ConfigurationIntentHandling {
     func provideDistrictOptionsCollection(for intent: ConfigurationIntent, with completion: @escaping (INObjectCollection<District>?, Error?) -> Void) {
         DataFetcher.shared.getDistrictData { data in
-            let districts = data.features.map { (feature) -> District in
+            var districts = data.features.map { (feature) -> District in
                 let district = District(identifier: feature.attributes.gen, display: feature.attributes.gen)
                 district.value = feature.attributes.objectId as NSNumber
                 return district
+            }
+            districts.sort {
+                $0.identifier! < $1.identifier!
             }
             let collection = INObjectCollection(items: districts)
             completion(collection, nil)
